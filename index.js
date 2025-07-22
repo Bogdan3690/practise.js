632c76baad555a6492be1d8400e31cf5632e5f69
Bogdan3690 <dblevko@gmail.com>
2025-07-22 12:58:44 -0600

    add types

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const name = "Bogdan";
-console.log(name);
+// console.log(name);
 const age = 2025;
 const getSume = age-25;
-console.log(getSume);
+// console.log(getSume);
 let user;
 console.log(user);
 user = "Bob";
 user = "John";
 console.log(user);
+// String
+const city = "Denver"
+const population = "716 577"
+const discription = "is a vibrant urban center in Colorado that seamlessly blends city life with outdoor recreation"
+console.log(typeof population)
+// number
+const number = 1.10;
+// boolean
+const isOnline = true;
+const isHiden = false;
+const shouldOpenChat = "true";
+console.log(typeof shouldOpenChat)
+console.log(typeof isOnline)
+// undefined
+
+// null
+const total = null
+console.log(typeof total)
+// symbol
+// conect to user
+// alert("hello world");
+// confirm("You sure you want to quit");
+
+const promtResult = prompt("subscribe");
+console.log(promtResult)
