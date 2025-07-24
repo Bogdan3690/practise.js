63c83f214f30de9963da9082302a9e547568191a
Bogdan3690 <dblevko@gmail.com>
2025-07-24 13:04:44 -0600

    practice with numbers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const name = "Bogdan";
-// console.log(name);
-const age = 2025;
-const getSume = age-25;
+console.log(name);
+// const age = 2025;
+// const getSume = age-25;
 // console.log(getSume);
 let user;
 console.log(user);
@@ -12,15 +12,17 @@ console.log(user);
 const city = "Denver"
 const population = "716 577"
 const discription = "is a vibrant urban center in Colorado that seamlessly blends city life with outdoor recreation"
-console.log(typeof population)
+discription.length
+console.log(population.length)
+console.log(typeof population);
 // number
 const number = 1.10;
 // boolean
 const isOnline = true;
 const isHiden = false;
 const shouldOpenChat = "true";
-console.log(typeof shouldOpenChat)
-console.log(typeof isOnline)
+console.log(typeof shouldOpenChat);
+console.log(typeof isOnline);
 // undefined
 
 // null
@@ -33,3 +35,13 @@ console.log(typeof total)
 
 // const promtResult = prompt("subscribe");
 // console.log(promtResult)
+
+const num1 = prompt("enter numbers");
+// const num2 = prompt("enter numbers");
+// const getNumbers = Number(num1) + Number(num2);
+// console.log(getNumbers);
+// console.log(typeof num1);
+
+// const age = prompt("Запитай у користувача його вік");
+// const getAge = 2025 - Number(age);
+// console.log("Result of variable age: ",getAge);
