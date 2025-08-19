615b7bcecccf433590ebe7bb96df4f9807ecc5ef
Bogdan3690 <dblevko@gmail.com>
2025-08-19 17:06:12 -0600

    add branch

--- branching.js
+++ branching.js
@@ -12,13 +12,56 @@ if(totalPrice > accaunt){
 
 // alert("Have a good day");
 
-const points = 30;
-if (points <= 10) {
-    console.log("level 1");
-} else if (points <= 20){
-    console.log("level 2");
-} else if (points <= 30){
-    console.log("level 3");
-} else {
-    console.log("High level")
-}
+// const points = 30;
+// if (points <= 10) {
+//     console.log("level 1");
+// } else if (points <= 20){
+//     console.log("level 2");
+// } else if (points <= 30){
+//     console.log("level 3");
+// } else {
+//     console.log("High level")
+// } 
+
+// 1
+// if ("0") {
+//  console.log( 'Привіт' );
+// }
+// true
+
+// 2
+// let question = prompt("Яка “офіційна” назва JavaScript?");
+// if (question === "ECMAScript"){
+//     alert("Правильно!")
+// }else{
+//     alert("Ви не знаєте? ECMAScript!")
+// }
+
+// 3
+// let sentence = Number(prompt("Enter number"));
+// if (sentence > 0){
+//     console.log("1");
+// }else if (sentence < 0){
+//     console.log("-1");
+// }else if(sentence === 0){
+//     console.log("0");
+// }
+// console.log(typeof sentence);
+
+// 4
+// let a = 1;
+// let b = 3;
+// let result = a + b < 4 ? 'Нижче' : "Вище";
+// console.log(result)
+
+// let segment = Number(prompt("Enter number"));
+// if (segment > ){
+//     console.log("1");
+// }
+// if (sentence < 0){
+//     console.log("-1");
+// }if(sentence === 0){
+//     console.log("0");
+// }if(sentence === 0){
+//     console.log("0");
+// }
