5f47e992450110f6c16d0e237b75692715da8bef
Bogdan3690 <dblevko@gmail.com>
2025-08-21 15:45:00 -0600

    add 2, 3, 4, problems

--- branching.js
+++ branching.js
@@ -66,18 +66,58 @@ if(totalPrice > accaunt){
 //     console.log("0");
 // }
 
-let isOnline = false;
-let isFriend = true;
-let isDnd = true;
-if (isOnline === true){
-    prompt("Enter message")}
-else if (isOnline === false){
-    alert("No online!")}
-if (isFriend === true){
-    prompt("Enter message")}
-else if (isFriend === false){
-    alert("No friend!")}
-if (isDnd === true){
-    prompt("Enter message")}
-else if (isDnd === false){
-    alert("With Dnd on!")}
+// let isOnline = true;
+// let isFriend = true;
+// let isDnd = false;
+// if (isOnline === true && isFriend === true && isDnd === false){
+//     prompt("Enter message")}
+// else{
+//     alert("No online!")}
+
+// let free = "free";
+// let pro = "pro";
+// let vip = "vip";
+// let ask = prompt("what subscribe?")
+// if (ask === pro || ask ===  vip){
+//     alert("Content")}
+// else if (ask === free){
+//     alert("Get a fee subscribe!")}
+
+// let balance = Number(prompt("How many money do u got"));
+// let payment = Number(prompt("Sum"));
+// if (balance >= payment){
+//     alert(`Загальна вартість замовлення ${payment} кредитів. Перевіряємо кількість доступних коштів на рахунку`)
+//     alert(`На рахунку залишилося ${balance - payment} кредитів`)
+// }else if(balance < payment)
+//     alert(`На рахунку недостатньо коштів для проведення операції!`)
+// alert("Операція завершена");
+
+// let stringOne = prompt("Enter content").trim();
+// let stringTwo = prompt("Enter content").trim();
+// if (stringOne != "" && stringTwo != ""){
+//     alert("Обидва поля заповнені")
+// }else{
+//     alert("Не всі поля заповнені")
+// }
+
+// let number1 = Number(prompt("enter number"));
+// let number2 = Number(prompt("enter second number"));
+// let sum = number1 + number2
+// if (sum > 10){
+//     alert("Сума більша за 10");
+// }else{
+//     alert("Сума менша або дорівнює 10")};
+
+// let text = prompt("Enter").toLowerCase()
+// if(text.includes("javascript")){
+//     alert("Текст містить слово JavaScript");
+// }else{
+//     alert("Текст не містить слово JavaScript")
+// }
+
+let numbers = Number(promt("Enter Numbers"))
+if (numbers >= 10 && numbers <= 20){
+    alert("Число входить в діапазон від 10 до 20")
+}else{
+    alert("Число не входить в діапазон від 10 до 20")
+}
