52f6fb0b93a46af3902d4cb756acf00756012e35
Bogdan3690 <dblevko@gmail.com>
2025-08-23 12:31:28 -0600

    replace hw

--- branching.js
+++ branching.js
@@ -91,33 +91,3 @@ if(totalPrice > accaunt){
 // }else if(balance < payment)
 //     alert(`На рахунку недостатньо коштів для проведення операції!`)
 // alert("Операція завершена");
-
-// let stringOne = prompt("Enter content").trim();
-// let stringTwo = prompt("Enter content").trim();
-// if (stringOne != "" && stringTwo != ""){
-//     alert("Обидва поля заповнені")
-// }else{
-//     alert("Не всі поля заповнені")
-// }
-
-// let number1 = Number(prompt("enter number"));
-// let number2 = Number(prompt("enter second number"));
-// let sum = number1 + number2
-// if (sum > 10){
-//     alert("Сума більша за 10");
-// }else{
-//     alert("Сума менша або дорівнює 10")};
-
-// let text = prompt("Enter").toLowerCase()
-// if(text.includes("javascript")){
-//     alert("Текст містить слово JavaScript");
-// }else{
-//     alert("Текст не містить слово JavaScript")
-// }
-
-let numbers = Number(promt("Enter Numbers"))
-if (numbers >= 10 && numbers <= 20){
-    alert("Число входить в діапазон від 10 до 20")
-}else{
-    alert("Число не входить в діапазон від 10 до 20")
-}
