689f4b38eb48d01ecaa8a1dfbfccaf76585dab7d
Bogdan3690 <dblevko@gmail.com>
2025-09-11 15:57:59 -0600

    Add

--- array.js
+++ array.js
@@ -1,5 +1,5 @@
 /*
- * Знайомство з масивами
+ * - 3найомство з масивами
  * - Объявлення
  * - Індексація
  * - Довжина
@@ -8,11 +8,11 @@
  * - Ітерація
  */
 
-const places = ['home', 'school', 'church', 'park']
-const lastIndex = places.length - 1
+// const places = ['home', 'school', 'church', 'park']
+// const lastIndex = places.length - 1
 // console.log(places[lastIndex])
 // console.log(places.length - 1)
-places[3] = 'garden'
+// places[3] = 'garden'
 // console.log(places)
 
 // places[0] += '!'
@@ -22,10 +22,59 @@ places[3] = 'garden'
 // console.log(places)
 
 // !
-
-for (let i = 0; i < places.length; i += 1) {
-    places[i] += '!'
-    console.log(places[i])
-}
-
+// for (let i = 0; i < places.length; i += 1) {
+//     places[i] += '!'
+//     console.log(places[i])
+// }
 // !
+
+/*
+ * Порахувати загальну суму покупок в корзині
+ */
+
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+// let total = 0;
+
+// for (let i = 0; i < cart.length; i += 1){
+//     cart[i] = Math.round(cart[i] * 1.1)
+//     console.log(cart[i])
+// }
+
+// for (const product of cart) {
+//     total += product
+// }
+// console.log(total)
+
+// Додати такс
+
+/*
+ * Напиши скрипт пошуку логіна
+ * - Якщо логіна немає, вивести повідомлення 'Користувач [логін] не знайдено.'
+ * - Якщо знайшли логін, вивести повідомлення 'Користувач [логін] знайдено.'
+ *
+ * - Потім через for...of
+ * - Логіка break
+ * - Метод includes() с тернарним оператором
+ */
+
+const logins = ['m4ngoDoge', 'aj4xth3m4n', 'k1widab3st', 'poly1scute'];
+const loginToFind = 'aj4xth3m4n';
+let message;
+
+
+if (logins.includes(loginToFind)){
+        message = `Користувач ${loginToFind} знайдено.`
+    }else{
+        message = `Користувач ${loginToFind} не знайдено.`
+    }
+
+
+// for (const login of logins) {
+//     if (login === loginToFind){
+//         message = `Користувач ${loginToFind} знайдено.`
+//         break
+//     }else{
+//         message = `Користувач ${loginToFind} не знайдено.`
+//     }
+// }
+// alert(message)
