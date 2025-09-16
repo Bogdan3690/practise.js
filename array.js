1cbf11fe1eca2478627a7bd30d74aea082abaa1e
Bogdan3690 <dblevko@gmail.com>
2025-09-16 15:25:26 -0600

    add methods

--- array.js
+++ array.js
@@ -57,16 +57,16 @@
  * - Метод includes() с тернарним оператором
  */
 
-const logins = ['m4ngoDoge', 'aj4xth3m4n', 'k1widab3st', 'poly1scute'];
-const loginToFind = 'aj4xth3m4n';
-let message;
+// const logins = ['m4ngoDoge', 'aj4xth3m4n', 'k1widab3st', 'poly1scute'];
+// const loginToFind = 'aj4xth3m4n';
+// let message;
 
 
-if (logins.includes(loginToFind)){
-        message = `Користувач ${loginToFind} знайдено.`
-    }else{
-        message = `Користувач ${loginToFind} не знайдено.`
-    }
+// if (logins.includes(loginToFind)){
+//         message = `Користувач ${loginToFind} знайдено.`
+//     }else{
+//         message = `Користувач ${loginToFind} не знайдено.`
+//     }
 
 
 // for (const login of logins) {
@@ -78,3 +78,50 @@ if (logins.includes(loginToFind)){
 //     }
 // }
 // alert(message)
+
+// arrays methods 16 Sep
+
+/*
+ * Напиши скрипт, який об"єднує всі елементи массива в один рядок.
+ * Елементів може бути довільна кількість.
+ * Нехай елементи массива  в рядку будут розділені комою.
+ */
+// let names = ['Bohdan', 'Irina', 'Dima']
+// console.log(names.join(', '))
+/*
+ * Робимо slug в URL з назви стратті (приклад на dev.to)
+ * Заголовок статті складається тільки з букв та пропусків
+ *
+ * - Нормалізуємо рядок
+ * - Разбиваємо по словах
+ * - Зшиваємо в рядок з розділителями
+ * - Ченінг
+ */
+// https://dev.to/devsatasurion/is-tailwind-css-accessible-52dc
+// Повинно вийти top-10-benefits-of-react-framework
+
+// Методи split() і join()
+
+// const title = 'Top 10 benefits of React framework';
+// let normalizeTitle = title.toLowerCase().split(' ').join('-')
+// console.log(normalizeTitle);
+
+// Методи indexOf() і includes()
+// +
+// Методи push(), pop(), shift(), unshift()
+
+// let fruits = ['banana', 'strawberry', 'blackberry', 'pineapple', 'watermelon']
+// fruit.shift()
+
+// Метод slice()
+
+// fruits.slice(1,1)
+// console.log(fruits.slice(2,4))
+
+// Метод concat()
+
+// let colors = ['red', 'black', 'green']
+// let dobleArray = colors.concat(fruits,names)
+// console.log(dobleArray)
+
+// Метод splice()
