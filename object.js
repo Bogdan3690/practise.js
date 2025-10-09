b1140506ab925382df136ff91af1dd62f5feee25
Bogdan3690 <dblevko@gmail.com>
2025-10-09 15:09:23 -0600

    add objects methods

--- object.js
+++ object.js
@@ -24,9 +24,64 @@ const pc = {
     components: ['motherboard', 'culer', 'videocard'],
     isOnSale: true
 }
-console.log(pc)
+// console.log(pc)
 
-console.log(pc.components)
+// console.log(pc.components)
 pc.isOnSale = false
-console.log(pc.isOnSale)
-console.log(pc.components.push('proccesor'))
+// console.log(pc.isOnSale)
+// console.log(pc.components.push('proccesor'))
+
+// lesson 2
+
+/*
+ * Методи об"екту і this при звернені до властивостей в методах
+ *
+ * - changeName
+ * - addTrack
+ * - updateRating
+ * - getTrackCount
+ */
+
+const playlist = {
+    name: 'Gym playlist',
+    tracks: ['track 1', 'track 2', 'track 3'],
+    rating: 9,
+    changeName(newName){
+        this.name = newName
+    },
+    addTrack(newTrack){
+        this.tracks.push(newTrack)
+    },
+    updateRating(newRating){
+        this.rating = newRating
+    },
+    getTrackCount(){
+        return this.tracks.length
+    }
+}
+playlist.changeName('Home music')
+playlist.addTrack('track 4')
+playlist.updateRating(4)
+console.log(playlist)
+playlist.getTrackCount()
+console.log(playlist.getTrackCount())
+
+const shirt = {
+    sizes: ['L', 'M', 'S'],
+    color: 'red',
+    label: 'Nike',
+    reports: {
+        good: 3,
+        middle: 5,
+        bad: 2,
+    },
+    changeLabel(newLabel){
+        this.label = newLabel
+    },
+    checkSizes(size){
+        return this.sizes.includes(size)
+    }
+}
+shirt.changeLabel('Adidas')
+console.log(shirt.checkSizes('L'))
+console.log(shirt)
