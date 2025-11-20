70e54e37b417915473bdc8104fafaddbae251f91
Bogdan3690 <dblevko@gmail.com>
2025-11-20 15:27:31 -0700

    add

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -1,14 +1,20 @@
-const imgElement = document.querySelector('.image')
-console.log(imgElement);
-console.log(imgElement.src);
-console.log(imgElement.alt)
-imgElement.alt = 'Nature'
-console.log(imgElement.alt)
-imgElement.width = '150'
-console.log(imgElement.width)
+// const imgElement = document.querySelector('.image')
+// console.log(imgElement);
+// console.log(imgElement.src);
+// console.log(imgElement.alt)
+// imgElement.alt = 'Nature'
+// console.log(imgElement.alt)
+// imgElement.width = '150'
+// console.log(imgElement.width)
 
 const imgElements = document.querySelectorAll('.image')
-console.log(imgElements)
-
 const listElement = document.querySelector('.site-nav')
-console.log(listElement);
+const linkElement = document.querySelector('a[href="https://"]')
+console.log(listElement.classList.add('new-class'));
+
+linkElement.textContent = 'hello'
+linkElement.style.border = '2px solid green'
+
+const firstItem = listElement.firstElementChild
+firstItem.textContent = '<a href="">New item</a>'
+firstItem.style.backgroundColor = 'yellow'
