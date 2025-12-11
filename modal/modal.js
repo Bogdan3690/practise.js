2b73c344980061b961acb702817411d593cc4b26
Bogdan3690 <dblevko@gmail.com>
2025-12-11 14:44:45 -0700

    finish modal

--- modal/modal.js
+++ modal/modal.js
@@ -7,25 +7,41 @@
 
 const openBtn = document.querySelector(['button[data-action="open-modal"'])
 const closeBtn = document.querySelector(['button[data-action="close-modal"'])
+const jsBackdrop = document.querySelector('.js-backdrop')
+
 openBtn.addEventListener('click', onOpenBtnClick)
+closeBtn.addEventListener('click', onCloseBtnClick)
+jsBackdrop.addEventListener('click', onBackDropClick)
+
 function onOpenBtnClick(){
     document.body.classList.add('show-modal')
-}
-closeBtn.addEventListener('click', onCloseBtnClick)
-function onCloseBtnClick(){
-    document.body.classList.remove('show-modal')
+    document.addEventListener('keydown', onEsc)
 }
 
-const jsBackdrop = document.querySelector('.js-backdrop')
-jsBackdrop.addEventListener('click', onBackDropClick)
+function onCloseBtnClick(){
+    onCloseModal()
+}
+
 function onBackDropClick(even){
     if (even.target === even.currentTarget) {
-    document.body.classList.remove('show-modal')
+        onCloseModal()
     }
     console.log(even.target);
     console.log(even.currentTarget);
 }
-console.log(openBtn.dataset);
-console.log(openBtn.dataset.action);
-openBtn.style.color=openBtn.dataset.color
 
+function onCloseModal(){
+    document.removeEventListener('keydown', onEsc)
+    document.body.classList.remove('show-modal')
+}
+
+// console.log(openBtn.dataset);
+// console.log(openBtn.dataset.action);
+// openBtn.style.color=openBtn.dataset.color
+
+function onEsc(event){
+    console.log(event.code);
+    if (event.code === 'Escape') {
+        onCloseModal()
+    }
+}
